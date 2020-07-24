9ba4e660dfa596a493eac752204c286dc7b1cd62
nester1van <nester1van@yandex.ru>
2020-07-24 19:35:06 +0400

    add logic to current videos layout

--- src/pages/search-page/ItemsLayout.js
+++ src/pages/search-page/ItemsLayout.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { setLayoutVideos } from '../../redux/appearance/actions';
+import './itesLayout.css';
 
-
-const ItemsLayout = ({ query, totalResults, setLayoutVideos }) => {
+const ItemsLayout = ({ query, totalResults, setLayoutVideos, layout }) => {
 
   const handleList = () => {
     setLayoutVideos('list');
@@ -15,17 +14,26 @@ const ItemsLayout = ({ query, totalResults, setLayoutVideos }) => {
   };
 
   return (
-    <>
-        Видео по запросу "{query}" <span>{totalResults}</span>
-      <Button onClick={handleList}>List</Button>
-      <Button onClick={handleGallery}>Gallery</Button>        
-    </>
+    <div className='layout'>
+      <p clasName='layout__query'>
+        Видео по запросу "{query}" 
+        <span className='layout__total-results'>{totalResults}</span></p>
+      <div className='layout__types'>
+      <span 
+        className={'layout__list' + (layout === 'list' ? ' layout__list_active' : '')}
+        onClick={handleList}></span>
+      <span 
+        className={'layout__gallery' + (layout === 'gallery' ? ' layout__gallery_active' : '')} 
+        onClick={handleGallery}></span>
+      </div>
+    </div>
   );
 };
 
 const mapStateToProps = (state) => ({
   query: state.search.query,
-  totalResults: state.search.data.pageInfo.totalResults
+  totalResults: state.search.data.pageInfo.totalResults,
+  layout: state.appearance.layout
 })
 
 export default connect(mapStateToProps, { setLayoutVideos })(ItemsLayout);
