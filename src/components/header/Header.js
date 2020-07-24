09c567290f0ab2fdef9ca97eb070b0e42fca5174
nester1van <nester1van@yandex.ru>
2020-07-24 18:55:21 +0400

    add logic to current route

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { Button, Container, Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import {setUserLogin, setUserPW} from '../../redux/user/actions';
@@ -16,6 +16,8 @@ const Header = ({token, setUserLogin, setUserPW, setToken}) => {
     localStorage.removeItem('userLogin');
   };
 
+  let pathName = useLocation().pathname;
+
   return (
     token ? 
     (<header className='header'>
@@ -23,8 +25,12 @@ const Header = ({token, setUserLogin, setUserPW, setToken}) => {
         <Row className='header__row'>
           <Col className='heder__col' md={5}>      
             <img className='header__logo' src={logo} alt='logo'/>
-            <Link className='header__item-search' to='/searchpage'>Поиск</Link>
-            <Link className='header__item-favorites' to='/favorites'>Избранное</Link></Col>
+            <Link 
+              className={'header__item-search' + (pathName === '/searchpage' ? ' header__item-search_active' : '')} 
+              to='/searchpage'>Поиск</Link>
+            <Link 
+              className={'header__item-favorites' + (pathName === '/favorites' ? ' header__item-favorites_active' : '')} 
+              to='/favorites'>Избранное</Link></Col>
           <Col className='heder__col' md={{span: 2, offset: 5}}>
             <Button className='header__item-exit' variant='outline-primary' onClick={handelClick}>Выйти</Button>
           </Col>
